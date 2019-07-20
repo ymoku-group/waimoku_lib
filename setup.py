550ac53560eb552cdd634bd36e6d2e9cfb768a7b
aseo <aseo@yahoo-corp.jp>
2019-07-20 13:41:05 +0900

    feat: setup.pyとそれに伴った各種ファイルの修正

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='waimoku',
-    version='0.0.1',
+    version='0.0.2',
     description='PythonScript to convert "Waimoku" participant information into "Yahoo! LODGE" participant list file format',
     author='Waimoku',
     maintainer='Waimoku',
