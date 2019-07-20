1fa53887ee7e93d36d4bc804c6ca7c20d65e6f2b
aseo <aseo@yahoo-corp.jp>
2019-07-20 15:01:14 +0900

    feat: README.rstファイルの追加とsetup.pyにてrstファイルを指定する旨の変更を追加

---
+++ README.rst
@@ -0,0 +1,21 @@
+waimoku
+=======
+
+ワイもくのConnpasで取得しているアンケートから運営に必要な情報を抽出するPythonのScript
+です。
+
+インストール
+------------
+
+pipを使ってこのパッケージはインストールすることができます
+
+::
+
+   pip install --upgrade waimoku
+
+ライセンス
+----------
+
+このソフトウェアは、MITライセンスのもとで公開されています。
+
+詳しくは\ `LICENSE <https://github.com/ymoku-group/waimoku_lib/blob/master/LICENSE>`__\ を御覧ください。
--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+from os import path
 from setuptools import setup
 from setuptools import find_packages
+
+
+def read(file_name: str):
+    """ファイルの内容を読み込む
+
+    Arguments:
+        fname {str} -- ファイル名
+
+    Returns:
+        [type] -- 読み込んだファイルの内容
+    """
+    return open(path.join(path.dirname(__file__), file_name)).read()
+
 
 setup(
     name='waimoku',
     version='0.0.3',
     description='PythonScript to convert "Waimoku" participant information into "Yahoo! LODGE" participant list file format',
+    long_description=read("README.rst"),
     author='Atsuki Seo',
     maintainer='Atsuki Seo',
     install_requires=["pandas", "openpyxl"],
